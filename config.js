48b3eeb81d6b36aa9d88d060957874cd2bab7ce6
mrdemona <136832570+mrdemona@users.noreply.github.com>
2023-07-15 07:03:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94740804536', 'Dinuwa Official²⁰²³', true],
-  ['94740804536', 'Dinuwa Official²⁰²³', true], 
+  ['94702314166', 'Demon Official²⁰²³', true],
+  ['94702314166', 'Demon Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
